b8c88792bd5ab6032492105474ae15de4fa3a984
Iuliia Drobotun <yudrobotun8@gmail.com>
2024-05-30 22:00:16 +0300

    Add validator.py

---
+++ validator/validator.py
@@ -0,0 +1,23 @@
+import autorization
+import blind_sign
+import datetime
+check_dict = {}
+
+data = autorization.autorization()
+response = autorization.tcp_connection_to_server(data).decode()
+if response == '0':
+    print('\n')
+    print('Вы авторизованы как валидатор.')
+    check_dict = blind_sign.upload_file()
+    target_date = datetime.datetime(2024, 5, 30)
+    while datetime.datetime.now() < target_date:
+        blind_sign.get_and_sign()
+elif response == '1':
+    print('\n')
+    print('Ошибка авторизации. Введен неверный код.')
+elif response == '2':
+    print('\n')
+    print('Ошибка авторизации. Введена неверная электронная почта.')
+else:
+    print('\n')
+    print('Неизвестный ответ сервера. Выход')
